0c11e459752ab2b971e582eb9afa9ba50caa3126
sbravor64 <Andresbravo97>
2019-10-04 18:56:50 +0200

    4 actividades hechas

--- src/com/company/MostraInfoDir.java
+++ src/com/company/MostraInfoDir.java
@@ -8,16 +8,24 @@ public class MostraInfoDir {
 
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+        String red="\033[31m";
+        String green="\033[32m";
+        String reset="\u001B[0m";
 
         System.out.println("Mostrar lista de Directorio");
         System.out.print("Escribe el path de un Directorio: ");
         String nomDirectori = scanner.nextLine();
 
         File d = new File(nomDirectori);
-        String [] directorios = d.list();
 
-        for (int i = 0; i <directorios.length ; i++) {
-            System.out.println(directorios[i]);
+        for (File file : d.listFiles()) {
+            if(!file.isHidden()){
+                if(file.isFile()){
+                    System.out.println(red+file.getName());
+                } else if (file.isDirectory()){
+                    System.out.println(green+file.getName()+reset);
+                }
+            }
         }
 
     }
