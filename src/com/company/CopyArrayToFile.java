7fe59273dc97b8d35f265beda89b81049d21748f
Andrés Bravo <sbravir642elpuig.xeill.net>
2019-10-16 12:24:00 +0200

    ejercicio 10 terminado

--- src/com/company/CopyArrayToFile.java
+++ src/com/company/CopyArrayToFile.java
@@ -11,14 +11,27 @@ public class CopyArrayToFile {
     public static void main(String[] args) throws IOException {
 
         String[] palabras={"hola","buenas","tardes"};
-        FileWriter fichero_destination = new FileWriter("/home/dam2a/Escriptori");
-        BufferedWriter = new BufferedWriter(fichero_destination);
 
+//        File fichero_destination = new File("/home/dam2a/Escriptori");
+        File fichero_destination = new File("C:\\Users\\Usuario\\Documents\\Copy\\prueba\\arraylist.txt");
+        FileWriter fw = new FileWriter(fichero_destination);
 
         for(String dato : palabras){
-
+            fw.write(dato+"\n");
         }
+        fw.close();
 
+        FileReader f = new FileReader(fichero_destination);
+        BufferedReader br = new BufferedReader(f);
+
+        boolean eof = false;
+        while (!eof){
+            String line=br.readLine();
+            if(line==null){
+                eof=true;
+            } else System.out.print(line+" ");
+        }
+        br.close();
 
     }
 }
