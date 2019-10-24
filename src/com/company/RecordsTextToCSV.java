2a45eff2072a237acd17e65f5d19d27919e8e7e8
sbravor64 <Andresbravo97>
2019-10-24 15:36:33 +0200

    hecho

--- src/com/company/RecordsTextToCSV.java
+++ src/com/company/RecordsTextToCSV.java
@@ -6,52 +6,68 @@ import java.util.List;
 
 public class RecordsTextToCSV  {
 
+    private static String[] nombres = new String[10];
+    private static String[] apellidos = new String[10];
+    private static String[] edades = new String[10];
+    private static String split=",";
+
     public static void main(String[] args) throws IOException {
+
 //      File fichero_destination = new File("C:\\Users\\Usuario\\Documents\\Copy\\prueba\\vocals.txt");
 
         File file_sortida = new File("/home/dam2a/Escriptori/sortida.csv");
+        BufferedReader fr_sortida = new BufferedReader(new FileReader(file_sortida));
 
         BufferedReader br = new BufferedReader(new FileReader("/home/dam2a/Escriptori/leidoporCSV"));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(file_sortida, true));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file_sortida));
 
         llegeixFitxer(br);
         escriuSortidaCSV(bw);
+        mostraPerPantalla(fr_sortida);
     }
 
     public static void llegeixFitxer(BufferedReader br) throws IOException{
 
-        String[] nombres = new String[10];
-        String[] apellidos = new String[10];
-        String[] edades = new String[10];
-
         String linea=br.readLine();
         int i=0, y=0, k=0;
 
-        String split;
         while(linea!=null){
-            split="----";
-            if (!linea.equals(split)) {
-                nombres[i]=linea;
-                linea=br.readLine();
-                i++;
-                apellidos [y]=linea;
-                linea=br.readLine();
-                y++;
-                edades[k]=linea;
-                linea=br.readLine();
-                k++;
-            }
+            nombres[i]=linea;
+            linea=br.readLine();
+            i++;
+            apellidos [y]=linea;
+            linea=br.readLine();
+            y++;
+            edades[k]=linea;
+            linea=br.readLine();
+            k++;
             linea=br.readLine();
         }
     }
 
     public static void escriuSortidaCSV(BufferedWriter bw) throws IOException {
-        bw.write("Esto es una prueba usando Buffered");
+
+        bw.write("nombre" + "," + "apellidos" + "," + "edad");
         bw.newLine();
-        bw.write("Seguimos usando Buffered");
+
+        for (int i = 0; i <2 ; i++) {
+            bw.write(nombres[i] + split);
+            bw.write(apellidos[i] + split);
+            bw.write(edades[i]);
+            bw.newLine();
+        }
+        bw.write("\n");
+        bw.close();
     }
 
-    public void mostraPerPantalla(){}
+    public static void mostraPerPantalla(BufferedReader br) throws IOException {
+        String linea = br.readLine();
+
+        while(linea!=null){
+            System.out.println(linea);
+            linea=br.readLine();
+        }
+    }
 
 }
 
