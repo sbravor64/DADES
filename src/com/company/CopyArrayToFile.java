1ef356f62694772c83bd7f8ec52fdeb2a082b2de
sbravor64 <Andresbravo97>
2019-10-24 16:26:44 +0200

    hecho

--- src/com/company/CopyArrayToFile.java
+++ src/com/company/CopyArrayToFile.java
@@ -7,23 +7,29 @@ import java.nio.file.StandardCopyOption;
 public class CopyArrayToFile {
 
     private static Object BufferedWriter;
+    private static String[] palabras={"hola","buenas","tardes"};
 
     public static void main(String[] args) throws IOException {
 
-        String[] palabras={"hola","buenas","tardes"};
-
-        File fichero_destination = new File("/home/dam2a/Escriptori");
+        File fichero_destination = new File("/home/dam2a/Escriptori/arraylist.txt");
 //        File fichero_destination = new File("C:\\Users\\Usuario\\Documents\\Copy\\prueba\\arraylist.txt");
         FileWriter fw = new FileWriter(fichero_destination);
-
-        for(String dato : palabras){
-            fw.write(dato+"\n");
-        }
-        fw.close();
 
         FileReader f = new FileReader(fichero_destination);
         BufferedReader br = new BufferedReader(f);
 
+        guardarArchivo(fw);
+        mostrarPantalla(br);
+    }
+
+    public static void guardarArchivo(FileWriter fw) throws IOException {
+        for(String dato : palabras){
+            fw.write(dato+"\n");
+        }
+        fw.close();
+    }
+
+    public static void mostrarPantalla(BufferedReader br) throws IOException {
         boolean eof = false;
         while (!eof){
             String line=br.readLine();
@@ -32,7 +38,6 @@ public class CopyArrayToFile {
             } else System.out.print(line+" ");
         }
         br.close();
-
     }
 }
 
