4f40d60d261064de71b82a7576edd0b200dbe32f
sbravor64 <Andresbravo97>
2019-10-28 15:45:13 +0100

    acabado

--- src/com/company/FileTypes.java
+++ src/com/company/FileTypes.java
@@ -6,21 +6,47 @@ public class FileTypes {
 
     public static void main(String[] args) throws IOException {
 
-        FileOutputStream sortida = new FileOutputStream("/home/dam2a/Documents/accesadades");
+
+        FileOutputStream sortida = new FileOutputStream("/home/dam2a/Documents/accesadades/fichero.txt");
         DataOutputStream dataOutputStream = new DataOutputStream(sortida);
 
         dataOutputStream.writeByte(22);
         dataOutputStream.writeBoolean(false);
         dataOutputStream.writeChar('z');
         dataOutputStream.writeDouble(1.5);
-        dataOutputStream.write(10);
         dataOutputStream.writeFloat(3.6f);
         dataOutputStream.writeLong(5);
         dataOutputStream.writeBytes("hola");
-    }
 
-    static void writeByte (byte b){
-        dataOutputStream.writeByte(22);
+        dataOutputStream.close();
+
+        FileReader fr = new FileReader("/home/dam2a/Documents/accesadades/fichero.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line=br.readLine();
+
+        while (line!=null){
+            System.out.println(line);
+            line=br.readLine();
+        }
+        fr.close();
+        br.close();
+
+        FileInputStream fileInputStream = new FileInputStream("/home/dam2a/Documents/accesadades/fichero.txt");
+        DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+
+
+        while (dataInputStream.available() > 0) {
+            System.out.println(dataInputStream.readByte());
+            System.out.println(dataInputStream.readBoolean());
+            System.out.println(dataInputStream.readChar());
+            System.out.println(dataInputStream.readDouble());
+            System.out.println(dataInputStream.readFloat());
+            System.out.println(dataInputStream.readLong());
+            System.out.println(dataInputStream.readLine());
+        }
+        fileInputStream.close();
+        dataInputStream.close();
     }
 }
 
